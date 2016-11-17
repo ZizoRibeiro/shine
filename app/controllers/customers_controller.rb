587a936d64a06983a9b75a9f9da808a8160344f9
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-11-16 21:12:57 -0800

    Added table address and materialized view to customer details

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -24,7 +24,8 @@ class CustomersController < ApplicationController
   end
 
   def show
-    customer = Customer.find(params[:id])
+    customer_detail = CustomerDetail.find(params[:id])
+    # customer = Customer.find(params[:id])
     respond_to do |format|
       format.json { render json: customer }
     end
---
+++ spec/factories/addresses.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :address do
+    street "MyString"
+    city "MyString"
+    state nil
+    zipcode "MyString"
+  end
+end
